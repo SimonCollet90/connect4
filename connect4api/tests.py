4ddaa13f4e09ff1d8c04dd721ecff711a7be7ce0
Simon <simon.127@hotmail.fr>
2026-04-03 17:56:24 +0200

    test: add app installation test

--- connect4api/tests.py
+++ connect4api/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class Connect4APIConfigTest(TestCase):
+    def test_app_is_installed(self):
+        from django.apps import apps
+        self.assertTrue(apps.is_installed('connect4api'))
